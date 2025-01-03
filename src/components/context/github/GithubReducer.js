6fbddd173e5873cab94c21e9f045f7c0c23c6123
kaankurcer <kkurcer@gmail.com>
2025-01-03 20:20:36 +0300

    feat: add clear functionality on clear button

--- src/components/context/github/GithubReducer.js
+++ src/components/context/github/GithubReducer.js
@@ -11,6 +11,11 @@ const githubReducer = (state, action) => {
         users: action.payload,
         loading: false,
       };
+    case "CLEAR_USERS":
+      return {
+        ...state,
+        users: [],
+      };
     default:
       return state;
   }
--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -4,7 +4,7 @@ import GithubContext from "../context/github/GithubContext";
 function UserSearch() {
   const [text, setText] = useState("");
 
-  const { users, searchUsers } = useContext(GithubContext);
+  const { users, searchUsers, clearUsers } = useContext(GithubContext);
 
   const handleChange = (e) => setText(e.target.value);
   const handleSubmit = (e) => {
@@ -18,6 +18,10 @@ function UserSearch() {
     }
 
     setText("");
+  };
+
+  const handleClear = () => {
+    clearUsers();
   };
 
   return (
@@ -45,7 +49,9 @@ function UserSearch() {
       </div>
       {users.length > 0 && (
         <div>
-          <button className="btn btn-ghost btn-lg">CLEAR</button>
+          <button onClick={handleClear} className="btn btn-ghost btn-lg">
+            CLEAR
+          </button>
         </div>
       )}
     </div>
